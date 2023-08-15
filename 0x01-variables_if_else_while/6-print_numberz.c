e4f7f03f8a0fedc55c4eb53d50cdd535e7f0f3f7
abdooo <ehabminiawy@gmail.com>
2023-08-15 13:18:51 -0700

    sf

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/**
+* main - Prints the alphabet
+*
+* Return: always 0 (Success)
+*/
+int main(void)
+{
+
+	int i;
+
+	for (i = 48; i < 58 ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
